24fc3c1d244a4d2628b0b6ef037c62a498b72f8c
Can <info@cancingi.com.tr>
2020-08-20 23:07:28 +0300

    email verification development

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,19 @@ const normalize = require('normalize-url');
 const rcSecretKey = '6LcrQbQZAAAAACpo8mGmKbUb2cRvx8I92VJ8jhCX';
 const { stringify } = require('querystring');
 const fetch = require('node-fetch');
+const nm = require('nodemailer');
 
 const User = require('../models/User');
 
 const { authMw, isAdmin } = require('../middlewares/auth');
 const checkObjectId = require('../middlewares/checkObjectId');
+
+/* const DOMAIN = 'sandboxe800f9c6832143bb8916a08dce741112.mailgun.org';
+const mg = require('mailgun-js')({
+  apiKey: '94908f5fc3de5fb8a3ae03db2a05d43f-203ef6d0-16774a90',
+  domain: DOMAIN,
+}); */
+
 /**
  * @route POST /api/users
  * @desc Register user
@@ -92,6 +100,103 @@ router.post(
     } catch (err) {
       console.error(err.message);
       res.status(500).json({ errors: [{ msg: 'Server error' }] });
+    }
+  }
+);
+
+/**
+ * @route POST /api/users/testRegister
+ * @desc Register user (for email verification test) - ReCaptcha is disabled
+ * @access Public
+ */
+router.post(
+  '/testRegister',
+  [
+    check('name', 'İsim boş bırakılamaz').not().trim().isEmpty(),
+    check('email', 'Geçerli bir e-posta girin').trim().isEmail(),
+    check('password', 'Şifre en az 6 karakter olmalı').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Bu e-posta adresi kullanılıyor' }] });
+      }
+
+      const activationToken = jwt.sign(
+        { email },
+        process.env.JWT_ACC_ACTIVATE,
+        { expiresIn: '30m' }
+      );
+      const transporter = nm.createTransport({
+        host: 'mail.lolyama.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'noreply@lolyama.com',
+          pass: 'QGnyhDp2wuCzmes',
+        },
+      });
+      const sendMail = await transporter.sendMail({
+        from: 'LY Turnuva 🏅 <noreply@lolyama.com>',
+        to: email,
+        subject: 'Hesap Onayı ✅',
+        html: `
+        <h2>LoL Yama Turnuva Sistemi</h2>
+        <h4>Hesabınızı Onaylayın</h4>
+          <p>
+            Kayıt olduğunuz için teşekkürler. Aşağıdaki butona tıklayarak hesabınızı
+            aktif hale getirebilirsiniz. Eğer butona tıklayamıyorsanız linki
+            kopyalayıp tarayıcınıza yapıştırın 😊
+          </p><br><br>
+          <a
+            href="${process.env.CLIENT_URL}/api/auth/activate/${activationToken}"
+            style="
+              background-color: rgb(220, 35, 15);
+              padding: 10px;
+              border-radius: 5px;
+              color: white;
+              text-decoration: none;
+              margin-bottom: 20px;
+            "
+          >
+            Onayla
+          </a><br><br>
+          ${process.env.CLIENT_URL}/api/auth/activate/${activationToken}
+        `,
+      });
+      const avatar = normalize(
+        gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm',
+        }),
+        { forceHttps: true }
+      );
+
+      user = new User({ name, email, password, avatar });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      console.log('Message sent: %s', sendMail.messageId);
+      return res.json({
+        msg: 'Üyeliğiniz oluşturuldu, lütfen e-posta adresinizi onaylayınız.',
+      });
+    } catch (err) {
+      console.error(err.message);
+      res
+        .status(500)
+        .json({ errors: [{ msg: 'Server error', err: err.message }] });
     }
   }
 );
